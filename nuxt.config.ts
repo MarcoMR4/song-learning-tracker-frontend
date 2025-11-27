89691f5865aa25e9636b6e34b914a4648af49963
Marco MR <marcomr9641@gmail.com>
2025-11-27 13:11:35 -0600

    feat: add supabase plugin to connect database url and key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,17 +31,19 @@ export default defineNuxtConfig({
   modules: ['nuxt-quasar-ui'],
   runtimeConfig: {
       public: {
-          quasarBrand: {
-              primary: '#1976D2',
-              secondary: '#26A69A',
-              accent: '#9C27B0',
-              dark: '#1d1d1d',
-              'dark-page': '#121212',
-              positive: '#21BA45',
-              negative: '#C10015',
-              info: '#31CCEC',
-              warning: '#F2C037'
-          }
+        quasarBrand: {
+            primary: '#1976D2',
+            secondary: '#26A69A',
+            accent: '#9C27B0',
+            dark: '#1d1d1d',
+            'dark-page': '#121212',
+            positive: '#21BA45',
+            negative: '#C10015',
+            info: '#31CCEC',
+            warning: '#F2C037'
+        },
+        supabaseUrl: process.env.SUPABASE_URL,
+        supabaseAnonKey: process.env.SUPABASE_KEY,
       }
   },
   css: [
