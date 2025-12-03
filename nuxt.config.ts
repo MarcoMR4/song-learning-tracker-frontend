be760eb32608c85d9fe66dd654cd43bb53e2a9bf
Marco MR <marcomr9641@gmail.com>
2025-12-02 18:47:20 -0600

    feat: Add icon and title to Web Header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Song Learning Tracker frontend',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/images/logo2.webp' }
+      ]
+    }
+  },
   srcDir: 'src/', 
   modules: [
     "@nuxtjs/supabase"
